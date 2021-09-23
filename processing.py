5cc4c1dcd0ecf6b93b1e7c28e45d8d867b403bbc
jkranabetter <jkranabetter@gmail.com>
2021-09-22 17:38:07 -0700

    Rearranging old code, start of bfclassifier class

---
+++ processing.py
@@ -0,0 +1,29 @@
+import numpy as np
+import csv
+
+def selectFeaturesToLists(self, featureNumbers, featuresFilename):
+    # add -1 to all features numbers to convert them to list indeces
+    theChosenOnes = np.add(featureNumbers, -1).tolist()
+    
+    with open(featuresFilename, 'r') as inp:
+        firstIteration = True
+        featureVectors = []
+        
+        for row in csv.reader(inp):
+            # if on first iteration, add the class index to the list of selected features
+            if( firstIteration ):
+                classIdx = len(row) - 1
+                theChosenOnes.append(classIdx)
+                firstIteration = False 
+
+            # extract only the selected features and their corresponding classes
+            featureVectors.append(list(np.array(row)[theChosenOnes]))
+
+        # format data for model
+        classList = []
+        featureNames = featureVectors.pop(0) # remove first element (table titles)
+
+        # remove the classes from the end of the list and append to their own
+        for r in featureVectors:
+            classList.append(r.pop())
+    return featureVectors, classList
