65ae3e6324a76285ffe19686e49b3ee9320b92e3
Joshua Kranabetter <59706058+jkranabetter@users.noreply.github.com>
2021-11-17 08:47:18 -0800

    Update bf_classifier.py

--- bf_classifier.py
+++ bf_classifier.py
@@ -16,7 +16,7 @@ class BFClassifier(object):
         self.features = [44, 69, 95, 188, 195, 430, 472, 500, 531, 536, 539, 542, 549, 561, 568, 577, 580]
         self.feature_names = ['lowLevel.pitch_instantaneous_confidence.dmean2', 'lowLevel.silence_rate_30dB.skew', 'lowLevel.spectral_crest.dvar2', 'lowLevel.spectral_spread.skew', 'lowLevel.spectral_strongpeak.skew', 'lowLevel.mfcc.dmean2.6', 'lowLevel.mfcc.mean.9', 'lowLevel.mfcc.stdev.11', 'lowLevel.sccoeffs.mean.5', 'lowLevel.sccoeffs.skew.4', 'lowLevel.sccoeffs.stdev.1', 'lowLevel.sccoeffs.stdev.4', 'lowLevel.scvalleys.dmean.5', 'lowLevel.scvalleys.dvar.5', 'lowLevel.scvalleys.mean.0', 'lowLevel.scvalleys.skew.3', 'lowLevel.scvalleys.stdev.0']
         featureVectors, classList, featureNames = partitionSelectData('datasets/features_BF200.csv', self.features)
-        print('first vector: ',featureVectors[0])
+
         # create model, scale the data using a pipeline 
         # computes the mean and standard deviation on the training set so as to be able to later re-apply the same transformation on the testing set
         self.pipe = make_pipeline(StandardScaler(), svm.SVC(kernel = 'rbf', C=1, probability = True))
