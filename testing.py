6674b4054665db0639151e36d1314a5201ef0633
Joshua Kranabetter <59706058+jkranabetter@users.noreply.github.com>
2021-11-17 09:34:38 -0800

    Add testing script

--- testing.py
+++ testing.py
@@ -1,10 +1,11 @@
 from segmenter import Segmenter
+import os
 
 s1 = Segmenter()
 
-file = 'BF200Corpus/bafoground/aiff/34.aiff'
-
-windowData = s1.segment(file)
-
-for item in windowData:
-    print('\n',item)
+for filename in os.listdir('Test6Sounds'):
+    print('\nrunning: ', filename)
+    directory = 'Test6Sounds/' + filename
+    windowData = s1.segment(directory)
+    for item in windowData:
+        print('\n',item)
