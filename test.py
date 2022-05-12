cec65d7d802010e653e8028ee106430a9734dc6c
Joshua Kranabetter <jkranabetter@gmail.com>
2022-05-12 13:52:07 -0700

    working

--- test.py
+++ test.py
@@ -8,4 +8,4 @@ output_folder = './' + corpusName + '/'
 
 recregion = song[0.0 * 1000:1.0 * 1000]  # cut the region
 awesome = recregion.fade_in(50).fade_out(50)  # fade in/out
-awesome.export(output_folder + 'r_11', format='wav')  # save to disk
+awesome.export(output_folder + 'r_11.wav', format='wav')  # save to disk
