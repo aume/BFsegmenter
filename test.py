1b0a1dd2c181e12daecc7df9ec8c1facde43a43e
Joshua Kranabetter <jkranabetter@gmail.com>
2022-05-12 14:01:42 -0700

    testing

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from pydub import AudioSegment
+import os
 
-file = 'PotteryInterview.wav'
+file = '52_bg.aiff'
+filename = os.path.basename(file)
 corpusName = 'SegmentedCorpus'  # dir to put segments
 
-song = AudioSegment.from_wav(file)
+print(filename)
+song = AudioSegment.from_file(filename, 'aiff')
 output_folder = './' + corpusName + '/'
 
 recregion = song[0.0 * 1000:1.0 * 1000]  # cut the region
 awesome = recregion.fade_in(50).fade_out(50)  # fade in/out
-awesome.export(output_folder + 'r_11.wav', format='wav')  # save to disk
+awesome.export(output_folder + 'r_11.aiff', format='aiff')  # save to disk
